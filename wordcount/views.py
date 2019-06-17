89397c1970c0fed683a724fb0328a6ddad99bd50
aerosix <1005925523@qq.com>
2019-06-17 16:07:05 +0800

    词频统计

--- wordcount/views.py
+++ wordcount/views.py
@@ -1,4 +1,4 @@
-from django.http import  HttpResponse
+from django.http import HttpResponse
 from django.shortcuts import render
 
 #def home(request):
@@ -6,3 +6,18 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def count(request):
+    #print(request)
+    #return HttpResponse('hello')
+    text = request.GET['text']
+    print(text)
+    result = {}
+    for i in text:
+        if i not  in result:
+            result[i] = 1
+        else:
+            result[i] += 1
+    print(result)
+    sorted(result.items(), key=lambda x:x[1], reverse=True)
+    return render(request, 'count.html', {'count_result': result.items()})
